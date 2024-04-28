c4ccc8e043dab160e42f9a4ae64bd8b772d11538
Jogender866 <jogendersingh866@gmail.com>
2024-04-28 14:04:44 +0530

    Initial Setup Completed

---
+++ Weather&Map_iOS_UIKIt/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Weather&Map_iOS_UIKIt
+//
+//  Created by Jogender Singh on 28/04/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // TODO: DEINIT
+    deinit{
+        print("LoginVC has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
+}
---
+++ Weather&Map_iOS_UIKIt/SystemFiles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather&Map_iOS_UIKIt
+//
+//  Created by Jogender Singh on 28/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD METHOD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // TODO: DEINIT
+    deinit{
+        print("ViewController has been REMOVED...!")
+    }
+    
+    // MARK: - ACTIONS
+}
+
